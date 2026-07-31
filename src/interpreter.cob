72e86fd5d677707378e58d4cd9051bfa8c9c4979
Electro98 <geny_turov@mail.ru>
2026-07-31 16:23:39 +0900

    Add stack for loops and clear operation

--- src/interpreter.cob
+++ src/interpreter.cob
@@ -7,7 +7,13 @@
            05 MEMORY-ROW OCCURS MEMORY-SIZE TIMES INDEXED BY MEMORY-PTR.
                10 MEMORY-C        BINARY-CHAR UNSIGNED.
        01 CODE-PTR                BINARY-LONG.
-      *>  Temporary values
+       01 Stack-Max-Size          constant 255.
+       01 Stack.
+           03 Stack-Pointer       BINARY-CHAR UNSIGNED.
+           03 Stack-Cur-Value     BINARY-LONG.
+           03 Stack-Memory        OCCURS Stack-Max-Size TIMES.
+                   05 filler      BINARY-LONG.
+    *>    Temporary values
        01 TEMPORARY-BUFFER.
            05 CUR-CHR             PIC X VALUE ZERO.
            05 OUT-CHR             PIC X VALUE ZERO.
@@ -40,12 +46,21 @@
            WHEN ">"
               SET MEMORY-PTR UP BY 1
            WHEN "["
-              IF MEMORY-C(MEMORY-PTR) IS ZERO THEN
-                 PERFORM 3200-SEARCH-LOOP-END
-              END-IF
+              EVALUATE            TRUE
+               *>   Very specific optimization for the common pattern '[-]'
+                 WHEN IN-CODE(CODE-PTR:3) IS EQUAL "[-]"
+                    MOVE ZERO TO MEMORY-C(MEMORY-PTR)
+                    ADD 2 to CODE-PTR
+                 WHEN MEMORY-C(MEMORY-PTR) IS ZERO
+                    PERFORM 3200-SEARCH-LOOP-END
+                 WHEN OTHER
+                    PERFORM a200-PUT-VALUE
+              END-EVALUATE,
            WHEN "]"
               IF MEMORY-C(MEMORY-PTR) IS NOT ZERO THEN
-                 PERFORM 3300-SEARCH-LOOP-START
+                 PERFORM a500-READ-VALUE
+              ELSE
+                 PERFORM a400-DROP-VALUE
               END-IF
            WHEN "."
               MOVE MEMORY-C(MEMORY-PTR) TO OUT-CHR-R
@@ -106,5 +121,36 @@
 
        3400-GET-CURRENT-CHAR.
            MOVE IN-CODE(CODE-PTR:1) TO CUR-CHR.
+
+       a100-INITIALIZE-STACK.
+           INITIALIZE Stack
+               REPLACING NUMERIC DATA BY ZEROS.
+
+       a200-PUT-VALUE.
+           IF Stack-Pointer IS EQUAL TO Stack-Max-Size THEN
+              DISPLAY "Stack overflow! Aborting..."
+              GOBACK
+           END-IF.
+           ADD 1 TO Stack-Pointer.
+           MOVE CODE-PTR TO Stack-Memory(Stack-Pointer).
+
+       a300-POP-VALUE.
+           PERFORM A500-READ-VALUE.
+           PERFORM A400-DROP-VALUE.
+
+       a400-DROP-VALUE.
+           IF Stack-Pointer IS ZERO THEN
+              DISPLAY "Error: Trying to drop nonexistent value from stack. Aborting..."
+              GOBACK
+           END-IF.
+           MOVE 0 TO Stack-Memory(Stack-Pointer).
+           SUBTRACT 1 FROM Stack-Pointer.
+
+       a500-READ-VALUE.
+           IF Stack-Pointer IS ZERO THEN
+              DISPLAY "Error: Trying to read nonexistent value from stack. Aborting..."
+              GOBACK
+           END-IF.
+           MOVE Stack-Memory(Stack-Pointer) TO CODE-PTR.
 
        END PROGRAM brainfuck-interpreter.
